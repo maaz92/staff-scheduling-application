c87bfd3cd6d6bc7b151f85117bf21077225eeca5
Mohammad Maaz Khan <khanmohammadmaaz@gmail.com>
2022-11-13 22:00:05 +0400

    Remove moment because of typescript error

--- src/middleware/validation/users/validatorSortedByAccumulatedWorkHours.ts
+++ src/middleware/validation/users/validatorSortedByAccumulatedWorkHours.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-import moment from 'moment';
+
 export const validatorSortedByAccumulatedWorkHours = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(moment('2012-05-22', 'YYYY-MM-DD', true).isValid()); //true
   return next();
 };
